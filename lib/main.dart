c9bd6edcba00bd6709f223e51b4c7b27aa657c56
João Murta <joao.m.murta@parceiros.nos.pt>
2025-02-05 15:38:38 +0000

    home screen design

--- lib/main.dart
+++ lib/main.dart
@@ -16,9 +16,197 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+      home: HomePage(title: 'Sources'),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<HomePage> createState() {
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    final Widget horizontalList1 = Container(
+      padding: const EdgeInsets.only(left: 20),
+      margin: const EdgeInsets.symmetric(vertical: 20),
+      height: 150,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: [
+          Row(
+            spacing: 8,
+            children: [
+              Container(
+                width: 150,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(124, 54, 33, 25),
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(
+                    color: const Color.fromRGBO(82, 49, 36, 1),
+                    width: 1.5,
+                  ),
+                ),
+              ),
+              Container(
+                width: 150,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(124, 54, 33, 25),
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(
+                    color: const Color.fromRGBO(82, 49, 36, 1),
+                    width: 1.5,
+                  ),
+                ),
+              ),
+              Container(
+                width: 150,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(124, 54, 33, 25),
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(
+                    color: const Color.fromRGBO(82, 49, 36, 1),
+                    width: 1.5,
+                  ),
+                ),
+              ),
+              Container(
+                width: 150,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(124, 54, 33, 25),
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(
+                    color: const Color.fromRGBO(82, 49, 36, 1),
+                    width: 1.5,
+                  ),
+                ),
+              ),
+              Container(
+                width: 150,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(124, 54, 33, 25),
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(
+                    color: const Color.fromRGBO(82, 49, 36, 1),
+                    width: 1.5,
+                  ),
+                ),
+              ),
+              Container(
+                width: 150,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(124, 54, 33, 25),
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(
+                    color: const Color.fromRGBO(82, 49, 36, 1),
+                    width: 1.5,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        centerTitle: false,
+        title: Text(
+          widget.title,
+          style: const TextStyle(
+            color: Color.fromRGBO(255, 172, 172, 0.895),
+            fontWeight: FontWeight.w200,
+            fontSize: 34,
+          ),
+        ),
+      ),
+      body: Center(
+        child: ListView(
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(left: 20),
+              child: Text(
+                'Section 1',
+                style: TextStyle(
+                  color: Color.fromRGBO(255, 172, 172, 0.895),
+                  fontWeight: FontWeight.w300,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            horizontalList1,
+            const Padding(
+              padding: EdgeInsets.only(left: 20),
+              child: Text(
+                'Section 2',
+                style: TextStyle(
+                  color: Color.fromRGBO(255, 172, 172, 0.895),
+                  fontWeight: FontWeight.w300,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            horizontalList1,
+            const Padding(
+              padding: EdgeInsets.only(left: 20),
+              child: Text(
+                'Section 3',
+                style: TextStyle(
+                  color: Color.fromRGBO(255, 172, 172, 0.895),
+                  fontWeight: FontWeight.w300,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            horizontalList1,
+            const Padding(
+              padding: EdgeInsets.only(left: 20),
+              child: Text(
+                'Section 4',
+                style: TextStyle(
+                  color: Color.fromRGBO(255, 172, 172, 0.895),
+                  fontWeight: FontWeight.w300,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            horizontalList1,
+            const Padding(
+              padding: EdgeInsets.only(left: 20),
+              child: Text(
+                'Section 5',
+                style: TextStyle(
+                  color: Color.fromRGBO(255, 172, 172, 0.895),
+                  fontWeight: FontWeight.w300,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            horizontalList1,
+            const Padding(
+              padding: EdgeInsets.only(left: 20),
+              child: Text(
+                'Section 6',
+                style: TextStyle(
+                  color: Color.fromRGBO(255, 172, 172, 0.895),
+                  fontWeight: FontWeight.w300,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            horizontalList1,
+          ],
         ),
       ),
     );
